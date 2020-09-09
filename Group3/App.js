67309ac747a3c89ec2f56447368f03c3cf997064
patmean08 <70728528+patmean08@users.noreply.github.com>
2020-09-09 18:07:30 -0400

    Add files via upload

---
+++ Group3/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Linking } from 'react-native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { globalStyles } from './styles/styles.js';
+
+//Importing different screens
+import Events from './Screens/Screen1';
+import Home from './Screens/Home';
+
+//used to set up Drawer and Stack Navigators
+const Drawer = createDrawerNavigator ();
+const Stack = createStackNavigator ();
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+      <View style={globalStyles.container}>
+        <ImageBackground source= {require( "../Group3/assets/KSU.jpg" )} style={{ width: '100%', height: '100%'}}>
+
+          {/* This block of code is used to set set up the drawers on the main page of the app
+              and link each screen together. */} 
+          <NavigationContainer>
+            <Drawer.Navigator>
+              <Drawer.Screen name = "Home" component = {Home} />
+              <Drawer.Screen name = "Events" component = {Events} />
+            </Drawer.Navigator>
+          </NavigationContainer> 
+
+       </ImageBackground>
+      </View>
+    );
+  }
+}
