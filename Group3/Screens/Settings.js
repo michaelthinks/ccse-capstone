71bbcfb191b73244f90864c5e3b287c8117d72a1
paulkruskamp <70982403+paulkruskamp@users.noreply.github.com>
2020-10-14 19:16:45 -0400

    final update 10/14

--- Group3/Screens/Settings.js
+++ Group3/Screens/Settings.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, Component } from "react";
 import {
-
   Text,
   View,
   Switch,
@@ -12,231 +11,233 @@ import {
 
 import { globalStyles } from "../styles/styles.js";
 import { DrawerActions } from "@react-navigation/native";
-import AsyncStorage from '@react-native-community/async-storage';
-
+import AsyncStorage from "@react-native-community/async-storage";
 
 export default class EventsList extends Component {
   state = {
     likedEventsSwitch: false,
     newEventsSwitch: false,
     SMSSwitch: false,
-    darkTheme: false    
+    darkTheme: false,
   };
 
-  componentDidMount(){
-    //this.state.darkTheme = 
+  componentDidMount() {
+    //this.state.darkTheme =
   }
 
   render() {
-    
     return (
-      
       <SafeAreaView style={globalStyles.mainContainer}>
         <StatusBar style="auto" hidden={false} />
 
         <View style={globalStyles.adjustedHeaderContainer}>
-          <TouchableWithoutFeedback onPress={() => this.props.navigation.dispatch(DrawerActions.openDrawer())}>
-              <View style={globalStyles.adjustedMenuIcon}>
-                  <Image source={require('../assets/menu.png')} />
-              </View>
+          <TouchableWithoutFeedback
+            onPress={() =>
+              this.props.navigation.dispatch(DrawerActions.openDrawer())
+            }
+          >
+            <View style={globalStyles.adjustedMenuIcon}>
+              <Image source={require("../assets/menu.png")} />
+            </View>
           </TouchableWithoutFeedback>
           <View style={globalStyles.adjustedHeaderText}>
-              <Text style={globalStyles.headerText}>Settings</Text>
+            <Text style={globalStyles.headerText}>Settings</Text>
           </View>
+        </View>
 
-      </View>
-
-        
-      <View style={globalStyles.contentContainer}>
-        <View style={{flexDirection: "column"}}>
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
+        <View style={globalStyles.contentContainer}>
+          <View style={{ flexDirection: "column" }}>
             <View
               style={{
-                height: 100,
-                width: "75%",
-                paddingLeft: 15,
+                flexDirection: "row",
               }}
             >
-              <Text
+              <View
                 style={{
-                  fontWeight: "bold",
+                  height: 100,
+                  width: "75%",
+                  paddingLeft: 15,
                 }}
               >
-                Receiving notifications for like events{" "}
-              </Text>
-              <Text>{this.state.likedEventsSwitch ? "Enabled" : "Disabled"}</Text>
-            </View>
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  Receiving notifications for like events{" "}
+                </Text>
+                <Text>
+                  {this.state.likedEventsSwitch ? "Enabled" : "Disabled"}
+                </Text>
+              </View>
 
-
-
-            <View
-              style={{
-                height: 100,
-                width: "25%",
-                paddingRight: 15,
-              }}
-            >
-              <Switch
-                value={this.state.likedEventsSwitch}
-                onValueChange={printState(this.state.likedEventsSwitch), (likedEventsSwitch) => this.setState({ likedEventsSwitch })
-                }
-              />
-            </View>
-          </View>
-
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <View
-              style={{
-                height: 100,
-                width: "75%",
-                paddingLeft: 15,
-              }}
-            >
-              <Text
+              <View
                 style={{
-                  fontWeight: "bold",
+                  height: 100,
+                  width: "25%",
+                  paddingRight: 15,
                 }}
               >
-                Receive notification for new events
-              </Text>
-              <Text>{this.state.newEventsSwitch ? "Enabled" : "Disabled"}</Text>
+                <Switch
+                  value={this.state.likedEventsSwitch}
+                  onValueChange={
+                    (printState(this.state.likedEventsSwitch),
+                    (likedEventsSwitch) => this.setState({ likedEventsSwitch }))
+                  }
+                />
+              </View>
             </View>
 
             <View
               style={{
-                height: 100,
-                width: "25%",
-                paddingRight: 15,
+                flexDirection: "row",
               }}
             >
-              <Switch
-                value={this.state.newEventsSwitch}
-                onValueChange={printState(this.state.newEventsSwitch), (newEventsSwitch) => this.setState({ newEventsSwitch })
-                }
-              />
-            </View>
-          </View>
-
-
-
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <View
-              style={{
-                height: 100,
-                width: "75%",
-                paddingLeft: 15,
-              }}
-            >
-              <Text
+              <View
                 style={{
-                  fontWeight: "bold",
+                  height: 100,
+                  width: "75%",
+                  paddingLeft: 15,
                 }}
               >
-                Receive events notifications via SMS
-              </Text>
-              <Text>{this.state.SMSSwitch ? "Enabled" : "Disabled"}</Text>
-            </View>
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  Receive notification for new events
+                </Text>
+                <Text>
+                  {this.state.newEventsSwitch ? "Enabled" : "Disabled"}
+                </Text>
+              </View>
 
-            <View
-              style={{
-                height: 100,
-                width: "25%",
-                paddingRight: 15,
-              }}
-            >
-              <Switch
-                value={this.state.SMSSwitch}
-                onValueChange={
-                  printState(this.state.SMSSwitch), 
-                  (SMSSwitch) => this.setState({ SMSSwitch })
-                }
-              />
-            </View>
-          </View>
-
-
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <View
-              style={{
-                height: 100,
-                width: "75%",
-                paddingLeft: 15,
-              }}
-            >
-              <Text
+              <View
                 style={{
-                  fontWeight: "bold",
+                  height: 100,
+                  width: "25%",
+                  paddingRight: 15,
                 }}
               >
-                Dark Theme
-              </Text>
-              <Text>{this.state.darkTheme ? "On" : "Off"}</Text>
+                <Switch
+                  value={this.state.newEventsSwitch}
+                  onValueChange={
+                    (printState(this.state.newEventsSwitch),
+                    (newEventsSwitch) => this.setState({ newEventsSwitch }))
+                  }
+                />
+              </View>
             </View>
 
             <View
               style={{
-                height: 100,
-                width: "25%",
-                paddingRight: 15,
+                flexDirection: "row",
               }}
             >
-              <Switch
-                value={this.state.darkTheme}   
-                onValueChange={
-                  printState(this.state.darkTheme),
-                  setSwitchState(this.state.darkTheme),
-                  getSwitchState(),
-                  (darkTheme) => this.setState({ darkTheme })
-                }
-              />
+              <View
+                style={{
+                  height: 100,
+                  width: "75%",
+                  paddingLeft: 15,
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  Receive events notifications via SMS
+                </Text>
+                <Text>{this.state.SMSSwitch ? "Enabled" : "Disabled"}</Text>
+              </View>
+
+              <View
+                style={{
+                  height: 100,
+                  width: "25%",
+                  paddingRight: 15,
+                }}
+              >
+                <Switch
+                  value={this.state.SMSSwitch}
+                  onValueChange={
+                    (printState(this.state.SMSSwitch),
+                    (SMSSwitch) => this.setState({ SMSSwitch }))
+                  }
+                />
+              </View>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+              }}
+            >
+              <View
+                style={{
+                  height: 100,
+                  width: "75%",
+                  paddingLeft: 15,
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  Dark Theme
+                </Text>
+                <Text>{this.state.darkTheme ? "On" : "Off"}</Text>
+              </View>
+
+              <View
+                style={{
+                  height: 100,
+                  width: "25%",
+                  paddingRight: 15,
+                }}
+              >
+                <Switch
+                  value={this.state.darkTheme}
+                  onValueChange={
+                    (printState(this.state.darkTheme),
+                    setSwitchState(this.state.darkTheme),
+                    getSwitchState(),
+                    (darkTheme) => this.setState({ darkTheme }))
+                  }
+                />
+              </View>
             </View>
           </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
     );
   }
 }
 
 const printState = (prop) => {
-    console.log(prop)
-}
+  console.log(prop);
+};
 
 const setSwitchState = async (prop) => {
   try {
-      await AsyncStorage.setItem('result', JSON.stringify(prop));
-      console.log("set: " + prop);
+    await AsyncStorage.setItem("result", JSON.stringify(prop));
+    console.log("set: " + prop);
   } catch (error) {
-      alert("set: " + error);
+    alert("set: " + error);
   }
-}
+};
 
 const getSwitchState = async () => {
   try {
-    const value = await AsyncStorage.getItem('result');
+    const value = await AsyncStorage.getItem("resulit");
     const valueParsed = JSON.parse(value);
     if (value !== null) {
       console.log("get: " + valueParsed);
     } else {
-      console.log("empty")
+      console.log("get: empty");
     }
-   } catch (error) {
-     alert("get " + error)
-   }
-}
+  } catch (error) {
+    alert("get " + error);
+  }
+};
